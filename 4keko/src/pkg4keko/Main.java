fdb214ab4bfedadfd4f8a1d4affb8e804eac2103
SilaDikgez <sila.dikgez@yahoo.com>
2017-11-01 12:43:24 +0200

    divide

--- 4keko/src/pkg4keko/Main.java
+++ 4keko/src/pkg4keko/Main.java
@@ -17,5 +17,7 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+    public int Bolme(int x, int y){
+        return x/y;
+    }
 }
